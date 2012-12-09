0f3e454b95aad2de1c387e713010dcc2ffafba30
MelvinVinas <melvin.vinas@gmail.com>
2012-12-09 01:42:11 +0100

    Update code/Classe/ListeClient.c

--- code/Classe/ListeClient.c
+++ code/Classe/ListeClient.c
@@ -9,6 +9,30 @@
 
 int ajouterClient(liste_client *liste, unsigned int id)
 {
+    case_liste_client *new = malloc(sizeof(*new));
+    
+    if (liste == NULL || new == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    new->idClient = id;
+    new->next = NULL;
+
+    if (liste->first != NULL) /* liste is not empty */
+    {
+        /* go at the end of liste */
+        case_liste_client *current = liste->first;
+        while (current->next != NULL)
+        {
+            current = current->next; // till the last case of liste
+        }
+        current->next = new;
+    }
+    else /* liste is empty, we will add the first case*/
+    {
+        liste->first = new;
+    }
 }
 
 /*
