464b873d835fb3002840842789d5ce95de527fa5
MelvinVinas <melvin.vinas@gmail.com>
2012-12-09 01:34:03 +0100

    Update code/Classe/ListeClient.h

--- code/Classe/ListeClient.h
+++ code/Classe/ListeClient.h
@@ -1,12 +1,18 @@
 #ifndef H_LISTE
 #define H_LISTE
 
-struct liste_client {
+typedef struct case_liste_client case_liste_client;
+struct case_liste_client {
 	unsigned int idClient;
 	unsigned int idSession;
 	unsigned int *next;
-} liste_client;
+};
 
+typedef struct liste_client liste_client;
+struct liste_client
+{
+    case_liste_client *first;
+};
 
 int ajouterClient(liste_client *liste, unsigned int id);
 int supprimerClient(liste_client *liste, unsigned int id);
