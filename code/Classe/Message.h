092ecfa6e0b59e083c4338814ed83b2a18f44e18
MelvinVinas <melvin.vinas@gmail.com>
2012-12-09 18:27:24 +0100

    Update code/Classe/Message.h

--- code/Classe/Message.h
+++ code/Classe/Message.h
@@ -1,22 +1,28 @@
 #ifndef H_MESSAGE
 #define H_MESSAGE
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
 struct info {
-	char *number;
-	char *id;
-	char *size;
-	char *name;
-	char *sha1;
-	char *date;
-	char *path;
-	char *user;
+char *number;
+char *id;
+char *size;
+char *name;
+char *sha1;
+char *date;
+char *path;
+char *user;
 } info;
 
 struct message {
-	char *type;
-	char *status;
-	char *sessionid;
-	char *user;
+char *type;
+char *status;
+char *sessionid;
+char *user;
 } message;
 
 char * toString(message mess);
